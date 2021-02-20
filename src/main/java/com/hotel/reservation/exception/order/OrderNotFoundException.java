e3a746660f31770c329f3c7a64587309a298ea5b
giorgi <giorgiodishvili>
2021-02-20 17:57:48 +0400

    fixed message

--- src/main/java/com/hotel/reservation/exception/order/OrderNotFoundException.java
+++ src/main/java/com/hotel/reservation/exception/order/OrderNotFoundException.java
@@ -3,6 +3,6 @@ package com.hotel.reservation.exception.order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Room Not Found")
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Order Not Found")
 public class OrderNotFoundException extends RuntimeException {
 }
