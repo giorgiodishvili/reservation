60ecda87139908f9e6856ac8d5abb0196cd7c32c
giorgi <giorgiodishvili>
2021-02-20 17:57:07 +0400

    added AOP

---
+++ src/main/java/com/hotel/reservation/aspect/ReservationLoggingAspect.java
@@ -0,0 +1,58 @@
+package com.hotel.reservation.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Aspect
+@Component
+@Slf4j
+public class ReservationLoggingAspect {
+
+
+    //setup pointcut declaration
+    @Pointcut("execution(* com.hotel.reservation.controller.*.*(..))")
+    private void forControllerPackage(){}
+
+    @Pointcut("execution(* com.hotel.reservation.service.*.*(..))")
+    private void forServicePackage(){}
+
+    @Pointcut("execution(* com.hotel.reservation.repository.*.*(..))")
+    private void forRepositoryPackage(){}
+
+    @Pointcut("forControllerPackage() || forServicePackage() || forRepositoryPackage()")
+    private void forAppFlow(){}
+    // add @Before annotation
+
+    @Before("forAppFlow()")
+    public void before(JoinPoint theJoinPoint){
+        // display methods we are calling
+        log.trace("@Before calling method : {}", theJoinPoint.getSignature().toShortString());
+        //display the arguments to the method
+        Object[] arguments = theJoinPoint.getArgs();
+
+        for(Object tempArg : arguments){
+            log.debug("arguments : {}", tempArg);
+        }
+    }
+
+//    @AfterReturning(
+//            pointcut = "forAppFlow()",
+//            returning = "theResult"
+//    )
+//    public void afterReturning(JoinPoint theJoinPoint,Object theResult){
+//        //display method we are returniing from
+//        log.trace("@After Returning from method : {}", theJoinPoint.getSignature().toShortString());
+//        //display data returned
+//        log.debug(" ==> result: {}", theResult);
+//    }
+
+    @AfterThrowing(pointcut = "forAppFlow()",throwing="theExc")
+    public void afterThrowingException (JoinPoint theJoinPoint, Throwable theExc){
+        log.trace("@AfterThrowing exception on : {}", theJoinPoint.getSignature().toShortString());
+        log.info(" ==> Exception Thrown: ", theExc);
+    }
+}
---
+++ src/main/java/com/hotel/reservation/config/AOPConfig.java
@@ -0,0 +1,9 @@
+package com.hotel.reservation.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AOPConfig {
+}
