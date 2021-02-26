5ba70c59a505ec0f3938a5f01d006bdf6fb8e0a5
giorgi <giorgiodishvili>
2021-02-27 02:10:48 +0400

    removed unused exception

--- src/main/java/com/hotel/reservation/exception/order/OrderIdMustBeZeroOrNullException.java
+++
@@ -1,8 +0,0 @@
-package com.hotel.reservation.exception.order;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Order id must be null or zero")
-public class OrderIdMustBeZeroOrNullException extends RuntimeException {
-}
--- src/main/java/com/hotel/reservation/exception/room/RoomIdMustBeZeroOrNullException.java
+++
@@ -1,8 +0,0 @@
-package com.hotel.reservation.exception.room;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Room id must be null or zero")
-public class RoomIdMustBeZeroOrNullException extends RuntimeException {
-}
