6d26ed25ce10032396de0456b16f349ec9899784
giorgi <giorgiodishvili>
2021-02-28 23:15:57 +0400

    tried to add authorize button

--- src/main/java/com/hotel/reservation/config/SwaggerConfig.java
+++ src/main/java/com/hotel/reservation/config/SwaggerConfig.java
@@ -5,12 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Arrays;
 import java.util.Collections;
+
+//TODO add authorize support to swagger
 
 @Configuration
 @EnableOpenApi
@@ -23,11 +26,29 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.hotel.reservation.controller"))
                 .paths(PathSelectors.any())
                 .build()
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(basicAuthScheme()))
                 .apiInfo(getApiInformation());
 //                .useDefaultResponseMessages(false)
 //                .globalResponseMessage(RequestMethod.GET, getCustomizedResponseMessages()
 //                );
     }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(Arrays.asList(basicAuthReference()))
+                .forPaths(PathSelectors.ant("/**"))
+                .build();
+    }
+
+    private SecurityScheme basicAuthScheme() {
+        return new BasicAuth("basicAuth");
+    }
+
+    private SecurityReference basicAuthReference() {
+        return new SecurityReference("basicAuth", new AuthorizationScope[0]);
+    }
+
 
     private ApiInfo getApiInformation() {
         return new ApiInfo("Room Reservation API",
