3d68819fc92ec981a5150d0448c2147057834fc4
giorgi <giorgiodishvili>
2021-03-06 22:18:13 +0400

    touch up

--- src/main/java/com/hotel/reservation/service/AppUserService.java
+++ src/main/java/com/hotel/reservation/service/AppUserService.java
@@ -19,6 +19,7 @@ import java.util.UUID;
 public class AppUserService implements UserDetailsService {
     private final static String USER_NOT_FOUND_MESSAGE =
             "user with email %s not found";
+
     private final AppUserRepository appUserRepository;
 
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -29,6 +30,7 @@ public class AppUserService implements UserDetailsService {
         return appUserRepository.findByEmail(email).orElseThrow(() ->
                 new UsernameNotFoundException(String.format(USER_NOT_FOUND_MESSAGE, email)));
     }
+
 
     public String signUpUser(AppUser appUser) {
         boolean userExists = appUserRepository.findByEmail(appUser.getEmail()).isPresent();
