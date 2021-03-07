c247e3d20d1d002e3f3cae17ef6244e0e5d0b3bc
giorgi <giorgiodishvili>
2021-03-07 13:06:32 +0400

    improved userAdapter

--- src/main/java/com/hotel/reservation/adapter/AppUserAdapter.java
+++ src/main/java/com/hotel/reservation/adapter/AppUserAdapter.java
@@ -1,6 +1,11 @@
 package com.hotel.reservation.adapter;
 
+import com.hotel.reservation.config.security.authority.AppUserRole;
 import com.hotel.reservation.entity.AppUser;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
 
 public class AppUserAdapter {
 
@@ -22,8 +27,43 @@ public class AppUserAdapter {
     Getters
      */
 
+    @ApiModelProperty(accessMode = ApiModelProperty.AccessMode.READ_ONLY, readOnly = true)
+    public Long getId(){
+        return appUser.getId();
+    }
 
+    @ApiModelProperty(accessMode = ApiModelProperty.AccessMode.READ_ONLY, readOnly = true)
+    public String getUsername(){
+        return appUser.getUsername();
+    }
+
+    public String getFirstName(){
+        return appUser.getFirstName();
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return appUser.getAuthorities();
+    }
+
+    public String getLastName(){
+        return appUser.getLastName();
+    }
+
+    public boolean isAccountNonExpired(){
+        return appUser.isAccountNonExpired();
+    }
+    public boolean isEnabled(){
+        return appUser.isEnabled();
+    }
     /*
     Setters
      */
+    public void setFirstName(String firstName){
+        appUser.setFirstName(firstName);
+    }
+
+    public void setLastName(String lastName){
+    appUser.setLastName(lastName);
+    }
+
 }
