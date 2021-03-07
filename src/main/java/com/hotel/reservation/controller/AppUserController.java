e568907fcf4a8709da111ffc4cc60984f0c8db1b
giorgi <giorgiodishvili>
2021-03-07 13:06:57 +0400

    added functionality to AppUserController.java

--- src/main/java/com/hotel/reservation/controller/AppUserController.java
+++ src/main/java/com/hotel/reservation/controller/AppUserController.java
@@ -1,8 +1,15 @@
 package com.hotel.reservation.controller;
 
+import com.hotel.reservation.adapter.ListAdapter;
+import com.hotel.reservation.adapter.OrderAdapter;
 import com.hotel.reservation.service.AppUserService;
+import com.hotel.reservation.service.OrderService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppUserController {
 
 
-    private AppUserService appUserService;
+    private final AppUserService appUserService;
+    private final OrderService orderService;
 
-
+    @GetMapping("{userId}")
+    public Page<OrderAdapter> findAllOrdersByUserId(@PathVariable("userId") Long userId, Pageable pageable){
+        return ListAdapter.createPageFromList(orderService.findAllOrdersByUser(userId),pageable);
+    }
 }
