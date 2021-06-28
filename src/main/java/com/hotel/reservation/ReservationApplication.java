156d42404a60bdccec46bd9fe91188296c213351
giorgi <giorgiodishvili>
2021-06-28 11:28:01 +0400

    heroku database config

--- src/main/java/com/hotel/reservation/ReservationApplication.java
+++ src/main/java/com/hotel/reservation/ReservationApplication.java
@@ -1,8 +1,14 @@
 package com.hotel.reservation;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
@@ -12,4 +18,13 @@ public class ReservationApplication {
         SpringApplication.run(ReservationApplication.class, args);
     }
 
+//    @Value("${spring.datasource.url}")
+//    private String dbUrl;
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        HikariConfig config = new HikariConfig();
+//        config.setJdbcUrl(dbUrl);
+//        return new HikariDataSource(config);
+//    }
 }
--- src/main/java/com/hotel/reservation/config/heroku/DatabaseConfig.java
+++ src/main/java/com/hotel/reservation/config/heroku/DatabaseConfig.java
@@ -1,23 +1,24 @@
-package com.hotel.reservation.config.heroku;
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class DatabaseConfig {
-
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-
-    @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dbUrl);
-        return new HikariDataSource(config);
-    }
-}
+//package com.hotel.reservation.config.heroku;
+//
+//import com.zaxxer.hikari.HikariConfig;
+//import com.zaxxer.hikari.HikariDataSource;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.stereotype.Component;
+//
+//import javax.sql.DataSource;
+//
+//@Component
+//public class DatabaseConfig {
+//
+//    @Value("${spring.datasource.url}")
+//    private String dbUrl;
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        HikariConfig config = new HikariConfig();
+//        config.setJdbcUrl(dbUrl);
+//        return new HikariDataSource(config);
+//    }
+//}
