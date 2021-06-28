6a02d4bda6f067606e37ffb1ee37fe92ece5f011
giorgi <giorgiodishvili>
2021-06-28 14:23:21 +0400

    update

--- src/main/java/com/hotel/reservation/config/security/authority/AppUserRole.java
+++ src/main/java/com/hotel/reservation/config/security/authority/AppUserRole.java
@@ -2,15 +2,17 @@ package com.hotel.reservation.config.security.authority;
 
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public enum AppUserRole {
-    ROLE_USER(Set.of(AppUserPermission.ROOM_READ, AppUserPermission.ROOM_TYPE_READ, AppUserPermission.ORDERS_READ, AppUserPermission.ORDERS_WRITE, AppUserPermission.ORDERS_UPDATE)),
+    ROLE_USER(new HashSet<>(Arrays.asList(AppUserPermission.ROOM_READ, AppUserPermission.ROOM_TYPE_READ, AppUserPermission.ORDERS_READ, AppUserPermission.ORDERS_WRITE, AppUserPermission.ORDERS_UPDATE))),
 
-    ROLE_ADMIN(Set.of(AppUserPermission.ORDERS_WRITE, AppUserPermission.ORDERS_READ, AppUserPermission.ORDERS_UPDATE, AppUserPermission.ORDERS_DELETE,
+    ROLE_ADMIN(new HashSet<>(Arrays.asList(AppUserPermission.ORDERS_WRITE, AppUserPermission.ORDERS_READ, AppUserPermission.ORDERS_UPDATE, AppUserPermission.ORDERS_DELETE,
             AppUserPermission.ROOM_TYPE_WRITE, AppUserPermission.ROOM_TYPE_READ, AppUserPermission.ROOM_TYPE_UPDATE, AppUserPermission.ROOM_TYPE_DELETE,
-            AppUserPermission.ROOM_READ, AppUserPermission.ROOM_WRITE, AppUserPermission.ROOM_UPDATE, AppUserPermission.ROOM_DELETE));
+            AppUserPermission.ROOM_READ, AppUserPermission.ROOM_WRITE, AppUserPermission.ROOM_UPDATE, AppUserPermission.ROOM_DELETE)));
 
     private final Set<AppUserPermission> permissions;
 
